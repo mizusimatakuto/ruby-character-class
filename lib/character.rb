b0f44f9eefe4f6b82757991c341944e05c5f8339
mizushimatakuto <mizutaku0630@gmail.com>
2019-10-18 11:49:54 +0900

    10/18 課題

---
+++ lib/character.rb
@@ -0,0 +1,13 @@
+
+class Character
+  attr_reader :name, :hp
+
+    def initialize(name, hp)
+      @name = name
+      @hp = hp
+    end
+
+  def dead?
+    @hp <= 0
+  end
+end
--- lib/hero.rb
+++ lib/hero.rb
@@ -1,13 +1,8 @@
+
+require_relative './character.rb'
 # 勇者 (Hero) のふるまいを表すクラス
-class Hero
-  attr_reader :name, :hp
+class Hero < Character
+  # ここに Hero 独自の実装を書く
 
-    def initialize(name, hp)
-      @name = name
-      @hp = hp
-    end
-
-  def dead?
-    @hp <= 0
-  end
 end
+
